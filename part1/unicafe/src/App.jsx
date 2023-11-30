d6088899000ef1931607b2fa5280b2c786a6ea85
Yumiao Zhang <yumiaozhang6@gmail.com>
2023-11-30 14:17:08 +1100

    unicafe submisson

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,35 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable react/prop-types */
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const Button = (props) => {
+  return <button onClick={props.onClick}>{props.text}</button>;
+};
+const StatisticLine = (props) => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <p>
+      {props.text} {props.value}
+    </p>
+  );
+};
+const Statistics = (props) => {
+  return (
+    <div>
+      <StatisticLine text='good' value={props.good} />
+      <StatisticLine text='neutral' value={props.neutral} />
+      <StatisticLine text='bad' value={props.bad} />
+      <StatisticLine
+        text='all'
+        value={props.good + props.neutral + props.bad}
+      />
+      <StatisticLine
+        text='average'
+        value={
+          (props.good - props.bad) / (props.good + props.bad + props.neutral)
+        }
+      />
+      <StatisticLine
+        text='positive'
+        value={`${((props.good / (props.good + props.bad + props.neutral)) * 100).toFixed(2)} %`}
+      />
+    </div>
+  );
+};
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [feedback, setFeedback] = useState(false);
+  const goodClick = () => {
+    setFeedback(true);
+    setGood((currentState) => (currentState += 1));
+  };
+  const neutralClick = () => {
+    setFeedback(true);
+    setNeutral((prev) => ++prev);
+  };
+  const badClick = () => {
+    setFeedback(true);
+    setBad((prev) => ++prev);
+  };
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={goodClick} text='good' />
+      <Button onClick={neutralClick} text='neutral' />
+      <Button onClick={badClick} text='bad' />
+      <h1>statistics</h1>
+      {feedback ? (
+        <Statistics good={good} neutral={neutral} bad={bad} />
+      ) : (
+        "No feedback given"
+      )}
+    </div>
+  );
+};
 
-export default App
+export default App;
